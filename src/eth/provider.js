02ab6e85c0c2b4fdc6aafd6c825848037beeae9a
mpdn99 <mpdn99@gmail.com>
2023-05-17 11:19:12 +0700

    update enviroment

--- src/eth/provider.js
+++ src/eth/provider.js
@@ -1,6 +1,6 @@
 ﻿import { JsonRpcProvider } from 'ethers';
 
-const MAIN_ENDPOINT = `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
+const MAIN_ENDPOINT = `https://polygon-mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`;
 
 export function createProvider() {
   return new JsonRpcProvider(MAIN_ENDPOINT, 137);
