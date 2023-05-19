36eb3167c0325666be1c8b1bd84a011558fb5687
mpdn99 <mpdn99@gmail.com>
2023-05-19 15:21:20 +0700

    test

--- src/eth/customContract.js
+++ src/eth/customContract.js
@@ -1,7 +1,7 @@
 import { Contract } from 'ethers';
 
 export async function createInstance(nftContractAddress, provider) {
-  const abi = fetch(`https://api.polygonscan.com/api?module=contract&action=getabi&address=${nftContractAddress}&apikey=${process.env.REACT_APP_POLYGONSCAN_KEY}`)
+  const abi = await fetch(`https://api.polygonscan.com/api?module=contract&action=getabi&address=${nftContractAddress}&apikey=${process.env.REACT_APP_POLYGONSCAN_KEY}`)
     .then(res => res.json())
     .then(res => JSON.parse(res.result));
   console.log('abi ', abi);
